3601980a2531ab52efb2c381c2b866e92acbd03b
Park Miso <alth7512@gmail.com>
2020-11-20 22:39:48 +0900

    fix: fbv to cbv

--- rooms/views.py
+++ rooms/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10, orphans=5)
-    rooms = paginator.page(int(page))
-    return render(request, "rooms/home.html", {"page": rooms})
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
+
